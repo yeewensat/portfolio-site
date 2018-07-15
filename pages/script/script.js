3c94a05940a9190627eb16957f030e298b7af44b
Yee Wen Sat <10148350+yeewensat@users.noreply.github.com>
2018-07-15 17:08:22 -0600

    added detail portfolio page for RSI branding project

--- pages/script/script.js
+++ pages/script/script.js
@@ -1,3 +1,5 @@
+var slideIndex = 1;
+
 /* toggle between adding and reoving the "responsive" class to navigation when the user clics on the icon */
 function toggleResponsiveMenu() {
 	var x = document.getElementById("topNavigation");
@@ -6,4 +8,33 @@ function toggleResponsiveMenu() {
 	} else {
 		x.className = "navigation"
 	}
+}
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) { 
+	  slides[i].display = "none";
+  };
+  
+for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  };
+
+  slides[slideIndex-1].display = "block";
+  dots[slideIndex-1].className += " active";
 }
